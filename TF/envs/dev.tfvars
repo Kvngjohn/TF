33b262d5738bd9ee12b260da8895115879a0d55d
kvngjohn <agopeoluwajohn@gmail.com>
2025-10-30 03:28:28 +0000

    Update TF configuration

--- TF/envs/dev.tfvars
+++ TF/envs/dev.tfvars
@@ -1,8 +1,4 @@
-project_name        = "rim-dev"
-location            = "eastus2"
-address_space       = "10.20.0.0/16"
-subnet_prefix       = "10.20.1.0/24"
-allow_rdp_from_cidr = "17.172.176.90/32" # your public IP /32
+project_name        = "rim-dev"# your public IP /32
 vm_size             = "Standard_B2ms"
 admin_username = "azureadmin"
 admin_password = "Coldstone@2424@"
@@ -12,3 +8,14 @@ tags = {
   owner       = "iac"
 }
 # DO NOT put admin_password here. Set it via TF_VAR_admin_password.
+
+rg_name             = "myResourceGroup"
+location           = "East US 2"
+vnet_name           = "myVnet"
+address_space       = ["10.0.0.0/16"]
+snet_name           = "mySubnet"
+subnet_prefix      = ["10.0.1.0/24"]
+nsg_name            = "myNSG"
+allow_rdp_from_cidr = "17.172.176.90/32"
+pip_name            = "myPublicIP"
+nic_name            = "myNIC"
--- TF/modules/Networking/variable.tf
+++ TF/modules/Networking/variable.tf
@@ -1,15 +1,54 @@
-variable "rg_name" { type = string }
-variable "location" { type = string }
-variable "tags" { type = map(string) }
+variable "rg_name" {
+  description = "The name of the resource group"
+  type        = string
+}
 
-variable "vnet_name" { type = string }
-variable "address_space" { type = list(string) }
+variable "location" {
+  description = "The location of the resources"
+  type        = string
+}
 
-variable "snet_name" { type = string }
-variable "subnet_prefix" { type = list(string) }
+variable "tags" {
+  description = "A map of tags to assign to resources"
+  type        = map(string)
+}
 
-variable "nsg_name" { type = string }
-variable "allow_rdp_from_cidr" { type = string }
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  type        = string
+}
 
-variable "pip_name" { type = string }
-variable "nic_name" { type = string }
+variable "address_space" {
+  description = "The address space of the virtual network"
+  type        = list(string)
+}
+
+variable "snet_name" {
+  description = "The name of the subnet"
+  type        = string
+}
+
+variable "subnet_prefix" {
+  description = "The address prefix for the subnet"
+  type        = list(string)
+}
+
+variable "nsg_name" {
+  description = "The name of the network security group"
+  type        = string
+}
+
+variable "allow_rdp_from_cidr" {
+  description = "The CIDR range for allowing RDP access"
+  type        = string
+}
+
+variable "pip_name" {
+  description = "The name of the public IP"
+  type        = string
+}
+
+variable "nic_name" {
+  description = "The name of the network interface"
+  type        = string
+}
