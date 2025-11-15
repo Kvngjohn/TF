6d3299639ebe53710b38ac08b08045a98e556408
kvngjohn <agopeoluwajohn@gmail.com>
2025-11-15 00:02:35 +0000

    apply config tffile

--- TF/modules/sql_database/variable.tf
+++ TF/modules/sql_database/variable.tf
@@ -1,10 +1,37 @@
-variable "project_name" {}
-variable "location" {}
-variable "resource_group_name" {}
-variable "tags" {}
+# Variables
+variable "project_name" {
+  description = "Project name prefix for resources"
+  type        = string
+}
 
-variable "sql_admin_username" {}
-variable "sql_admin_password" {}
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for resources"
+  type        = map(string)
+}
+
+variable "sql_admin_username" {
+  description = "SQL Server admin username"
+  type        = string
+}
+
+variable "sql_admin_password" {
+  description = "SQL Server admin password"
+  type        = string
+  sensitive   = true
+}
+
 variable "database_sku" {
-  default = "Standard_S0"
+  description = "SKU for the SQL Database"
+  type        = string
+  default     = "Standard_S0"
 }
